2bd529d3c5b57bbfc41ecf8ebb36b848d9eb74b5
gokutek <vxer@qq.com>
2018-03-07 09:07:05 +0800

    DepthFirstPaths：图深度优先搜索，寻找路径

---
+++ DepthFirstPaths/DepthFirstPaths.h
@@ -0,0 +1,75 @@
+﻿#ifndef DEPTH_FIRST_PATHS_H
+#define DEPTH_FIRST_PATHS_H
+
+#include <vector>
+#include <limits>
+#include "Graphs/Graphs.h"
+
+
+/*
+===============================================================================
+简单修改DepthFirstSearch，使得可以获取到某个点的一条路径（非最短路径）
+===============================================================================
+*/
+class DepthFirstPaths
+{
+public:
+	DepthFirstPaths(const Graphs& G, size_t s);
+
+	// 是否存在到s到v的路径
+	bool HasPathTo(size_t v) const;
+
+	// 获取s到v的路径（非最短）
+	std::vector<size_t> PathTo(size_t v) const;
+
+private:
+	void DFS(const Graphs& G, size_t v);
+
+private:
+	std::vector<bool>	marked_;	///< 顶点的访问标志
+	std::vector<size_t>	edge_;		///< edge_[v]表示通过哪个顶点到达v的
+	const size_t		start_;		///< 起点
+};
+
+
+
+inline DepthFirstPaths::DepthFirstPaths(const Graphs& G, size_t s)
+	: start_(s)
+{
+	marked_.resize(G.V());
+	edge_.resize(G.V(), std::numeric_limits<size_t>::max());
+	DFS(G, s);
+}
+
+
+inline bool DepthFirstPaths::HasPathTo(size_t v) const
+{
+	return marked_[v];
+}
+
+
+inline std::vector<size_t> DepthFirstPaths::PathTo(size_t v) const
+{
+	std::vector<size_t> res;
+	while (v != start_) {
+		res.push_back(v);
+		v = edge_[v];
+	}
+	res.push_back(start_);
+	std::reverse(res.begin(), res.end());
+	return res;
+}
+
+
+inline void DepthFirstPaths::DFS(const Graphs& G, size_t v)
+{
+	marked_[v] = true;
+	for (auto w : G.Adj(v)) {
+		if (!marked_[w]) {
+			edge_[w] = v;
+			DFS(G, w);
+		}
+	}
+}
+
+#endif // DEPTH_FIRST_PATHS_H
---
+++ DepthFirstPaths/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <assert.h>
+#include <fstream>
+#include "DepthFirstPaths.h"
+
+
+int main()
+{
+	std::ifstream ifs("../data/tinyCG.txt");
+	assert(ifs.is_open());
+	size_t V;
+	ifs >> V;
+	Graphs G(V);
+	size_t E;
+	ifs >> E;
+	for (size_t i = 0; i < E; ++i) {
+		size_t v, w;
+		ifs >> v >> w;
+		G.AddEdge(v, w);
+	}
+
+	DepthFirstPaths search(G, 0);
+	for (size_t i = 0; i < G.V(); ++i) {
+		if (!search.HasPathTo(i)) { continue; }
+		auto vec = search.PathTo(i);
+		std::cout << "0 to " << i << ": ";
+		for (auto v : vec) {
+			std::cout << v << " ";
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}

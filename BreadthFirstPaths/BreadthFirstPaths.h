5bf512f35c06bb089325d3aeb772eeefb7e15b8c
gokutek <vxer@qq.com>
2018-03-08 08:38:54 +0800

    图：广度优先搜索

---
+++ BreadthFirstPaths/BreadthFirstPaths.h
@@ -0,0 +1,81 @@
+﻿#ifndef BREADTH_FIRST_PATHS_H
+#define BREADTH_FIRST_PATHS_H
+
+#include <vector>
+#include <queue>
+#include "Graphs/Graphs.h"
+
+/*
+===============================================================================
+广度优先搜索：
+可以用于查找最短路径
+===============================================================================
+*/
+class BreadthFirstPaths
+{
+public:
+	BreadthFirstPaths(const Graphs& G, size_t s);
+
+	// 是否存在到s到v的路径
+	bool HasPathTo(size_t v) const;
+
+	// 获取s到v的路径（非最短）
+	std::vector<size_t> PathTo(size_t v) const;
+
+private:
+	void BFS(const Graphs& G, size_t v);
+
+private:
+	std::vector<bool>	marked_;	///< 顶点的访问标志
+	std::vector<size_t>	edge_;		///< edge_[v]表示通过哪个顶点到达v的
+	const size_t		start_;		///< 起点
+};
+
+
+
+inline BreadthFirstPaths::BreadthFirstPaths(const Graphs& G, size_t s)
+	: start_(s)
+{
+	marked_.resize(G.V());
+	edge_.resize(G.V());
+	BFS(G, s);
+}
+
+
+inline bool BreadthFirstPaths::HasPathTo(size_t v) const
+{
+	return marked_[v];
+}
+
+
+inline std::vector<size_t> BreadthFirstPaths::PathTo(size_t v) const
+{
+	std::vector<size_t> res;
+	while (v != start_) {
+		res.push_back(v);
+		v = edge_[v];
+	}
+	res.push_back(start_);
+	std::reverse(res.begin(), res.end());
+	return res;
+}
+
+
+inline void BreadthFirstPaths::BFS(const Graphs& G, size_t v)
+{
+	std::queue<size_t> vertices;
+	vertices.push(v);
+	while (!vertices.empty()) {
+		v = vertices.front();
+		vertices.pop();
+		for (auto w : G.Adj(v)) {
+			if (!marked_[w]) {
+				marked_[w] = true;
+				edge_[w] = v;
+				vertices.push(w);
+			}
+		}
+	}
+}
+
+#endif // BREADTH_FIRST_PATHS_H
---
+++ BreadthFirstPaths/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <fstream>
+#include <iostream>
+#include "BreadthFirstPaths.h"
+
+
+int main()
+{
+	std::ifstream ifs("../data/tinyCG.txt");
+	assert(ifs.is_open());
+	size_t V;
+	ifs >> V;
+	Graphs G(V);
+	size_t E;
+	ifs >> E;
+	for (size_t i = 0; i < E; ++i) {
+		size_t v, w;
+		ifs >> v >> w;
+		G.AddEdge(v, w);
+	}
+
+	BreadthFirstPaths search(G, 0);
+	for (size_t i = 0; i < G.V(); ++i) {
+		if (!search.HasPathTo(i)) { continue; }
+		auto vec = search.PathTo(i);
+		std::cout << "0 to " << i << ": ";
+		for (auto v : vec) {
+			std::cout << v << " ";
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}

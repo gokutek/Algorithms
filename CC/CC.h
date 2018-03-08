cbad39b41161ad484143a032f895d71a8946578f
gokutek <vxer@qq.com>
2018-03-08 20:57:26 +0800

    图：求连通分量

---
+++ CC/CC.h
@@ -0,0 +1,78 @@
+﻿#ifndef CC_H
+#define CC_H
+
+#include <vector>
+#include "Graphs/Graphs.h"
+
+/*
+===============================================================================
+CC是connected components的缩写，表示连通分量
+===============================================================================
+*/
+class CC
+{
+public:
+	// 构造函数，预处理找出所有的连通分量
+	CC(const Graphs& G);
+	
+	// v和w是否连通
+	bool Connected(size_t v, size_t w) const;
+	
+	// 连通分量数
+	size_t Count() const;
+
+	// v所在的连通分量标识符（0 ~ count()-1）
+	size_t Id(size_t v) const;
+
+private:
+	void DFS(const Graphs& G, size_t v);
+
+private:
+	std::vector<size_t>	marked_;	///< 访问标识
+	size_t				count_;		///< 连通分量数
+};
+
+
+
+inline CC::CC(const Graphs& G)
+{
+	marked_.resize(G.V(), 0);
+	count_ = 0;
+	for (size_t i = 0; i < marked_.size(); ++i) {
+		if (!marked_[i]) {
+			++count_;
+			DFS(G, i);
+		}
+	}
+}
+
+
+inline bool CC::Connected(size_t v, size_t w) const
+{
+	return marked_[v] == marked_[w];
+}
+
+
+inline size_t CC::Count() const
+{
+	return count_;
+}
+
+
+inline size_t CC::Id(size_t v) const
+{
+	return marked_[v] - 1;
+}
+
+
+inline void CC::DFS(const Graphs& G, size_t v)
+{
+	marked_[v] = count_;
+	for (auto w : G.Adj(v)) {
+		if (!marked_[w]) {
+			DFS(G, w);
+		}
+	}
+}
+
+#endif // CC_H
---
+++ CC/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <fstream>
+#include <iostream>
+#include <assert.h>
+#include "CC.h"
+
+
+int main()
+{
+	std::ifstream ifs("../data/tinyG.txt");
+	assert(ifs.is_open());
+	size_t V;
+	ifs >> V;
+	Graphs G(V);
+	size_t E;
+	ifs >> E;
+	for (size_t i = 0; i < E; ++i) {
+		size_t v, w;
+		ifs >> v >> w;
+		G.AddEdge(v, w);
+	}
+
+	CC cc(G);
+
+	std::vector<std::vector<size_t>> components;
+	components.resize(cc.Count());
+	for (size_t i = 0; i < G.V(); ++i) {
+		components[cc.Id(i)].push_back(i);
+	}
+
+	for (size_t i = 0; i < components.size(); ++i) {
+		for (auto v : components[i]) {
+			std::cout << v << " ";
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}

a41e6205147156b06a90438a557441d443b5dc5a
gokutek <vxer@qq.com>
2018-04-25 08:53:26 +0800

    有向图：广度优先搜索，最短路径

---
+++ BreadthFirstDirectedPaths/BreadthFirstDirectedPaths.h
@@ -0,0 +1,85 @@
+﻿#ifndef BREADTH_FIRST_DIRECTED_PATHS_H
+#define BREADTH_FIRST_DIRECTED_PATHS_H
+
+#include <vector>
+#include <limits>
+#include <queue>
+#include <assert.h>
+#include "Digraph/Digraph.h"
+
+/*
+===============================================================================
+4.2.3：有向图中，一点到另一点的最短路径
+1. BFS不是递归的；
+===============================================================================
+*/
+class BreadthFirstDirectedPaths
+{
+public:
+	BreadthFirstDirectedPaths(Digraph const &G, size_t source);
+
+	// 是否存在到s到v的路径
+	bool HasPathTo(size_t v) const;
+
+	// 获取s到v的路径（非最短）
+	std::vector<size_t> PathTo(size_t v) const;
+
+private:
+	void BFS(Digraph const &G, size_t v);
+
+private:
+	size_t const		source_;
+	std::vector<bool>	marked_;
+	std::vector<size_t> edgeTo_;
+};
+
+
+
+inline BreadthFirstDirectedPaths::BreadthFirstDirectedPaths(Digraph const &G, size_t source)
+	: source_(source)
+{
+	marked_.resize(G.V(), false);
+	edgeTo_.resize(G.V(), std::numeric_limits<size_t>::max());
+	BFS(G, source);
+}
+
+
+inline bool BreadthFirstDirectedPaths::HasPathTo(size_t v) const
+{
+	return marked_[v];
+}
+
+
+inline std::vector<size_t> BreadthFirstDirectedPaths::PathTo(size_t v) const
+{
+	std::vector<size_t> res;
+	assert(HasPathTo(v));
+	while (v != source_)
+	{
+		res.push_back(v);
+		v = edgeTo_[v];
+	}
+	res.push_back(source_);
+	std::reverse(res.begin(), res.end());
+	return res;
+}
+
+
+inline void BreadthFirstDirectedPaths::BFS(Digraph const &G, size_t v)
+{
+	std::queue<size_t> q;
+	q.push(v);
+	while (!q.empty()) {
+		v = q.front();
+		q.pop();
+		for (size_t w : G.Adj(v)) {
+			if (!marked_[w]) {
+				marked_[w] = true;
+				edgeTo_[w] = v;
+				q.push(w);
+			}
+		}
+	}
+}
+
+#endif // BREADTH_FIRST_DIRECTED_PATHS_H

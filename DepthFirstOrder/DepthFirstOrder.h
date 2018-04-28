05ece6511d87f3589b3809233b7250943b2cb977
gokutek <vxer@qq.com>
2018-04-28 19:07:06 +0800

    改成vector

--- DepthFirstOrder/DepthFirstOrder.h
+++ DepthFirstOrder/DepthFirstOrder.h
@@ -1,9 +1,10 @@
 ﻿#ifndef DEPTH_FIRST_ORDER_H
 #define DEPTH_FIRST_ORDER_H
 
-#include <queue>
 #include <stack>
 #include <vector>
+#include <iterator>
+#include <algorithm>
 #include "Digraph/Digraph.h"
 
 /*
@@ -18,18 +19,18 @@ class DepthFirstOrder
 public:
 	DepthFirstOrder(Digraph const &G);
 
-	std::queue<size_t> const & PreOrder() const;
-	std::queue<size_t> const & PostOrder() const;
-	std::stack<size_t> const & ReversePostOrder() const;
+	std::vector<size_t> const & PreOrder() const;
+	std::vector<size_t> const & PostOrder() const;
+	std::vector<size_t> const & ReversePostOrder() const;
 
 private:
 	void DFS(Digraph const &G, size_t v);
 
 private:
 	std::vector<size_t>	marked_;
-	std::queue<size_t>	pre_;
-	std::queue<size_t>	post_;
-	std::stack<size_t>	reversePost_;
+	std::vector<size_t>	pre_;
+	std::vector<size_t>	post_;
+	std::vector<size_t>	reversePost_;
 };
 
 
@@ -42,36 +43,38 @@ inline DepthFirstOrder::DepthFirstOrder(Digraph const &G)
 			DFS(G, v);
 		}
 	}
+
+	reversePost_ = post_;
+	std::reverse(reversePost_.begin(), reversePost_.end());
 }
 
 
 inline void DepthFirstOrder::DFS(Digraph const &G, size_t v)
 {
 	marked_[v] = true;
-	pre_.push(v);
+	pre_.push_back(v);
 	for (size_t w : G.Adj(v)) {
 		if (!marked_[w]) {
 			DFS(G, w);
 		}
 	}
-	post_.push(v);
-	reversePost_.push(v);
+	post_.push_back(v);
 }
 
 
-inline std::queue<size_t> const & DepthFirstOrder::PreOrder() const
+inline std::vector<size_t> const & DepthFirstOrder::PreOrder() const
 {
 	return pre_;
 }
 
 
-inline std::queue<size_t> const & DepthFirstOrder::PostOrder() const
+inline std::vector<size_t> const & DepthFirstOrder::PostOrder() const
 {
 	return post_;
 }
 
 
-inline std::stack<size_t> const & DepthFirstOrder::ReversePostOrder() const
+inline std::vector<size_t> const & DepthFirstOrder::ReversePostOrder() const
 {
 	return reversePost_;
 }

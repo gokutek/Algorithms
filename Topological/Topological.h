2a00a7e8ba6257e85fd50854ff645f34c6d9e9fc
gokutek <vxer@qq.com>
2018-04-28 19:09:04 +0800

    有向图拓扑排序

---
+++ Topological/Topological.h
@@ -0,0 +1,52 @@
+﻿#ifndef TOPOLOGICAL_H
+#define TOPOLOGICAL_H
+
+#include <vector>
+#include "DirectedCycle/DirectedCycle.h"
+#include "DepthFirstOrder/DepthFirstOrder.h"
+
+/*
+===============================================================================
+4.2.4.2：有向图拓扑排序
+1. 有向图中没有环时才可以进行拓扑排序
+===============================================================================
+*/
+class Topological
+{
+public:
+	Topological(Digraph const &G);
+
+	// G是有向无环图吗
+	bool IsDAG() const;
+
+	// 拓扑有序的所有顶点
+	std::vector<size_t> const & GetOrder() const;
+
+private:
+	std::vector<size_t>	order_;
+};
+
+
+
+inline Topological::Topological(Digraph const &G)
+{
+	DirectedCycle cycleFinder(G);
+	if (!cycleFinder.HasCycle()) {
+		DepthFirstOrder dfs(G);
+	 	order_ = dfs.ReversePostOrder();
+	}
+}
+
+
+bool Topological::IsDAG() const
+{
+	return !order_.empty();
+}
+
+
+std::vector<size_t> const & Topological::GetOrder() const
+{
+	return order_;
+}
+
+#endif // TOPOLOGICAL_H
---
+++ Topological/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "Topological.h"
+#include "SymbolDigraph/SymbolDigraph.h"
+
+
+int main()
+{
+	SymbolDigraph sg("../data/jobs.txt", '/');
+	Topological top(sg.GetGraph());
+	auto &order = top.GetOrder();
+	for (auto v : order) {
+		std::cout << sg.NameOf(v) << std::endl;
+	}
+	std::cout << std::endl;
+	return 0;
+}

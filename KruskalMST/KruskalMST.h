6d7b3de190f7d9e03bbeebb95f4cec52299d4b6b
gokutek <vxer@qq.com>
2018-05-08 21:03:48 +0800

    最小生成树KruskalMST

--- KruskalMST/KruskalMST.h
+++ KruskalMST/KruskalMST.h
@@ -2,10 +2,18 @@
 #define KRUSKAL_MST_H
 
 #include <queue>
-#include <list>
+#include <vector>
 #include <functional>
 #include "EdgeWeightedGraph/EdgeWeightedGraph.h"
+#include "UF/UF.h"
 
+/*
+===============================================================================
+4.3.6：最小生成树Kruskal算法
+1. 依赖于前面实现的UF算法；
+2. 代码看起来比Prim算法更简单；
+===============================================================================
+*/
 class KruskalMST
 {
 	template<typename T>
@@ -14,25 +22,51 @@ class KruskalMST
 public:
 	explicit KruskalMST(EdgeWeightedGraph const &G);
 
+	// 最小生成树的所有边
+	std::vector<Edge> const& Edges() const;
+
+	// 最小生成树的权重
+	double Weight() const;
+
 private:
-	std::list<Edge> mst_;
+	std::vector<Edge> mst_;
+	double			weight_;
 };
 
 
 
 inline KruskalMST::KruskalMST(EdgeWeightedGraph const &G)
+	: weight_(0)
 {
 	MinPQ<Edge> pq;
 	for (Edge const &e : G.Edges()) {
 		pq.push(e);
 	}
 
+	UF uf(G.V());
 	while (!pq.empty() && mst_.size() < G.V() - 1) {
 		Edge const e = pq.top();
 		pq.pop();
 		size_t const v = e.Either();
 		size_t const w = e.Other(v);
+		if (!uf.Connected(v, w)) {
+			uf.Union(v, w);
+			mst_.push_back(e);
+			weight_ += e.Weight();
+		}
 	}
+}
+
+
+inline std::vector<Edge> const& KruskalMST::Edges() const
+{
+	return mst_;
+}
+
+
+inline double KruskalMST::Weight() const
+{
+	return weight_;
 }
 
 #endif // KRUSKAL_MST_H
--- KruskalMST/main.cpp
+++ KruskalMST/main.cpp
@@ -1,6 +1,14 @@
-﻿#include "KruskalMST.h"
+﻿#include <iostream>
+#include "KruskalMST.h"
 
 int main()
 {
+	EdgeWeightedGraph g("../data/tinyEWG.txt");
+	KruskalMST mst(g);
+	std::cout << "weight: " << mst.Weight() << std::endl;
+	auto &edges = mst.Edges();
+	for (auto e : edges) {
+		std::cout << e.Either() << ":" << e.Other(e.Either()) << " " << e.Weight() << std::endl;
+	}
 	return 0;
 }

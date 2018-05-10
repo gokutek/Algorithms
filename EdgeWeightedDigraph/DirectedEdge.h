2bdcd7ab736944c7e4cf62f3706605201b379bfe
gokutek <vxer@qq.com>
2018-05-10 08:48:17 +0800

    EdgeWeightedDigraph：加权有向图数据结构

---
+++ EdgeWeightedDigraph/DirectedEdge.h
@@ -0,0 +1,47 @@
+﻿#ifndef DIRECTED_EDGE_H
+#define DIRECTED_EDGE_H
+
+class DirectedEdge
+{
+public:
+	DirectedEdge(size_t v, size_t w, double weight);
+
+	size_t From() const;
+	
+	size_t To() const;
+
+	double Weight() const;
+
+private:
+	size_t const v_;
+	size_t const w_;
+	double const weight_;
+};
+
+
+inline DirectedEdge::DirectedEdge(size_t v, size_t w, double weight)
+	: v_(v)
+	, w_(w)
+	, weight_(weight)
+{
+}
+
+
+inline size_t DirectedEdge::From() const
+{
+	return v_;
+}
+
+
+inline size_t DirectedEdge::To() const
+{
+	return w_;
+}
+
+
+inline double DirectedEdge::Weight() const
+{
+	return weight_;
+}
+
+#endif // DIRECTED_EDGE_H
---
+++ EdgeWeightedDigraph/EdgeWeightedDigraph.h
@@ -0,0 +1,91 @@
+﻿#ifndef EDGE_WEIGHTED_DIGRAPH_H
+#define EDGE_WEIGHTED_DIGRAPH_H
+
+#include <vector>
+#include <list>
+#include "DirectedEdge.h"
+
+class EdgeWeightedDigraph
+{
+public:
+	explicit EdgeWeightedDigraph(size_t V);
+
+	void AddEdge(DirectedEdge const &e);
+
+	size_t V() const;
+
+	size_t E() const;
+
+	std::list<DirectedEdge> const& Adj(size_t v) const;
+
+	size_t OutDegree(size_t v) const;
+
+	size_t InDegree(size_t v) const;
+
+	std::list<DirectedEdge> Edges() const;
+
+private:
+	size_t									V_;
+	size_t									E_;
+	std::vector<std::list<DirectedEdge> >	adj_;
+	std::vector<size_t>						inDegree_;
+};
+
+
+inline EdgeWeightedDigraph::EdgeWeightedDigraph(size_t V)
+{
+	V_ = V;
+	E_ = 0;
+	adj_.resize	(V);
+	inDegree_.resize(V, 0);
+}
+
+
+inline void EdgeWeightedDigraph::AddEdge(DirectedEdge const &e)
+{
+	adj_[e.From()].push_back(e);
+	++inDegree_[e.To()];
+	++E_;
+}
+
+
+inline size_t EdgeWeightedDigraph::V() const
+{
+	return V_;
+}
+
+
+inline size_t EdgeWeightedDigraph::E() const
+{
+	return E_;
+}
+
+
+inline std::list<DirectedEdge> const& EdgeWeightedDigraph::Adj(size_t v) const
+{
+	return adj_[v];
+}
+
+
+inline size_t EdgeWeightedDigraph::OutDegree(size_t v) const
+{
+	return adj_[v].size();
+}
+
+
+inline size_t EdgeWeightedDigraph::InDegree(size_t v) const
+{
+	return inDegree_[v];
+}
+
+
+inline std::list<DirectedEdge> EdgeWeightedDigraph::Edges() const
+{
+	std::list<DirectedEdge> res;
+	for (std::list<DirectedEdge> const &adj : adj_) {
+		res.insert(res.end(), adj.begin(), adj.end());
+	}
+	return res;
+}
+
+#endif // EDGE_WEIGHTED_DIGRAPH_H
